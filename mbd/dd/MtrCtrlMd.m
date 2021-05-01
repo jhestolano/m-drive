3a99ee141eedae50f51bfbc28d5b54663113bde4
pickman22 <jhestolano@gmail.com>
2021-04-30 21:03:44 -0600

    Add step dir control mode enum.
    This control mode has not been fully implemented yet.
    
    Rename VoltCtrlMd -> PwmCtrlMd.

--- mbd/dd/MtrCtrlMd.m
+++ mbd/dd/MtrCtrlMd.m
@@ -2,11 +2,12 @@ classdef MtrCtrlMd < Simulink.IntEnumType
     %MTRCTLMD Controller mode.
     %   Defines controller mode: position, speed and current modes.
     enumeration
-      VoltCtrlMd(0),
+      PwmCtrlMd(0),
       IfbkCtrlMd(1),
       TrqCtrlMd(2),
       SpdCtrlMd(3),
       PosCtrlMd(4),
+      StepDirCtrlMd(5),
       OffCtrlMd(255),
     end
 end
